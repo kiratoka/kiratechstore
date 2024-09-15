000e176fc54999150065ee74ccc1f8779f561238
unknown <kiratoka2@gmail.com>
2024-09-15 23:21:56 +0700

    percobaan ke 7 atau bisa dibilang percobaan terakhir karena sekarang jam 23:21

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: 'next',
+  root: true,
+  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  ignorePatterns: ['**/payload-types.ts'],
+  plugins: ['prettier'],
   rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off',
-  },
-};
+    "prettier/prettier": [
+      "error", { endOfLine : "auto"}
+    ]
+  }
+}
